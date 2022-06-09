9e1d6e262ce8f0fa56b6c69acd521655aec92a02
CaptainJack <captain_jack_mail@163.com>
2022-06-09 18:33:43 +0800

    引入root-check对用户做降级处理

--- packages/core/lib/core.js
+++ packages/core/lib/core.js
@@ -1,5 +1,17 @@
-function core() {
-  console.log('this is core')
+const { log } = require('@pearl-cli/utils')
+
+async function core() {
+  await checkRoot()
+}
+
+/**
+ * 检查当前账户是否是root账户，如果是root用户，需要对root用户做降级
+ * 当 process.geteuid() 为0时，说明使用的是root用户. ps:不同的平台的普通用户的pid不一样
+ *
+ */
+async function checkRoot() {
+  await import('root-check')
+  log.info('用户权限:', process.geteuid())
 }
 
 module.exports = core
