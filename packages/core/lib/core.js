4ff0f103edf85be5f75c70186c3701016e9a8130
CaptainJack <captain_jack_mail@163.com>
2022-06-09 21:03:42 +0800

    利用semver设置用户最低的node版本环境

--- packages/core/lib/core.js
+++ packages/core/lib/core.js
@@ -1,7 +1,18 @@
+const semver = require('semver')
+const colors = require('colors')
+
 const { log } = require('@pearl-cli/utils')
+const { LOWEST_NODE_VERSION } = require('./constants')
 
 async function core() {
-  await checkRoot()
+  try {
+    await checkRoot()
+    checkNodeVersion()
+  } catch (err) {
+    if (err.message) {
+      log.error(err.message)
+    }
+  }
 }
 
 /**
@@ -12,6 +23,15 @@ async function core() {
 async function checkRoot() {
   await import('root-check')
   log.info('用户权限:', process.geteuid())
+}
+
+// 设置最低版本号，对比用户当前运行的 node 版本是否可以运行
+function checkNodeVersion() {
+  if (!semver.gte(process.version, LOWEST_NODE_VERSION)) {
+    throw new Error(
+      colors.red(`当前node版本为${process.version}，node版本不低于 ${LOWEST_NODE_VERSION}`)
+    )
+  }
 }
 
 module.exports = core
